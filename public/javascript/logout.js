2d509f50b0076f0d05e5577b1f69b9b6fb54d725
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-07-24 13:27:00 -0400

    test

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -5,7 +5,7 @@ async function logout() {
     });
   
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/index');
     } else {
       alert(response.statusText);
     }
